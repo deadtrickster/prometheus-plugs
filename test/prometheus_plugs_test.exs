d3a567722984515945a1c0fe610e4a52b679c069
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-28 15:13:03 +0300

    plug pipeline instrumenter tests

--- test/prometheus_plugs_test.exs
+++ test/prometheus_plugs_test.exs
@@ -1,9 +1,60 @@
 defmodule PrometheusPlugsTest do
   use ExUnit.Case
-  doctest Plug.PrometheusCollector
-  doctest Plug.PrometheusExporter
+  use Plug.Test
+  ## doctest Plug.PrometheusCollector
+  ## doctest Plug.PrometheusExporter
+
+  require Prometheus.Registry
+
+  setup do
+    # Prometheus.Registry.clear(:default)
+    # Prometheus.Registry.clear(:qwe)
+
+    Prometheus.TestPlugPipelineInstrumenter.setup()
+    Prometheus.TestPlugPipelineInstrumenterCustomConfig.setup()
+    Prometheus.TestPlugExporter.setup()
+    Prometheus.TestPlugExporterCustomConfig.setup()
+
+    :ok
+  end
+
+  use Prometheus.Metric
 
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+
+  defp call(conn) do
+    Prometheus.TestPlugStack.call(conn, Prometheus.TestPlugStack.init([]))
+  end
+
+  test "Plug Pipeline Instrumenter tests" do
+    conn = call(conn(:get, "/"))
+    assert conn.resp_body == "Hello World!"
+
+    assert 1 == Counter.value([name: :http_requests_total,
+                               registry: :default,
+                               labels: ['success', "GET", "www.example.com", :http]])
+
+    assert 1 == Counter.value([name: :http_requests_total,
+                               registry: :qwe,
+                               labels: ["GET", 12]])
+
+    assert {buckets, sum} = Histogram.value([name: :http_request_duration_microseconds,
+                                             registry: :default,
+                                             labels: ['success', "GET", "www.example.com", :http]])
+
+    assert sum > 0    
+    assert 13 = length(buckets)
+    assert 1 = Enum.reduce(buckets, fn(x, acc) -> x + acc end)
+
+    assert {buckets, sum} = Histogram.value([name: :http_request_duration_microseconds,
+                                             registry: :qwe,
+                                             labels: ["GET", 12]])
+
+    assert sum > 0
+    assert 3 = length(buckets)
+    assert 1 = Enum.reduce(buckets, fn(x, acc) -> x + acc end)
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,52 @@
 ExUnit.start()
+
+defmodule Prometheus.TestPlugPipelineInstrumenter do
+  use Prometheus.PlugPipelineInstrumenter
+end
+
+Application.put_env(:prometheus, Prometheus.TestPlugPipelineInstrumenterCustomConfig,
+  labels: [:method, :resp_length],
+  duration_buckets: [10, 100],
+  registry: :qwe)
+
+defmodule Prometheus.TestPlugPipelineInstrumenterCustomConfig do
+  use Prometheus.PlugPipelineInstrumenter
+
+  defp label_value(:resp_length, conn) do
+    String.length(conn.resp_body)
+  end
+end
+
+defmodule Prometheus.TestPlugExporter do
+  use Prometheus.PlugExporter
+end
+
+defmodule Prometheus.TestPlugExporterCustomConfig do
+  use Prometheus.PlugExporter
+end
+
+defmodule HelloWorldPlug do
+  import Plug.Conn
+
+  def init(options) do
+    # initialize options
+
+    options
+  end
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello World!")
+  end
+end
+
+defmodule Prometheus.TestPlugStack do
+  use Plug.Builder
+
+  plug Prometheus.TestPlugPipelineInstrumenter
+  plug Prometheus.TestPlugPipelineInstrumenterCustomConfig
+  plug Prometheus.TestPlugExporter
+  plug Prometheus.TestPlugExporterCustomConfig
+  plug HelloWorldPlug, []
+end
