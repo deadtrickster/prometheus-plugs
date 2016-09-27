ed462eaf5f5cf08e558eb59a3fca74adcef5a900
Ilya Khaprov <dead.trickster@gmail.com>
2016-09-27 11:10:56 +0300

    update prometheus.erl and bump to 1.0.0-rc1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PrometheusPlugs.Mixfile do
   use Mix.Project
 
-  @version "1.0.0-alpha8"
+  @version "1.0.0-rc1"
 
   def project do
     [app: :prometheus_plugs,
@@ -43,7 +43,7 @@ defmodule PrometheusPlugs.Mixfile do
      {:plug, "~> 1.0"},
      {:ex_doc, "~> 0.11", only: :dev},
      {:earmark, ">= 0.0.0", only: :dev},
-     {:prometheus_ex, "~> 1.0.0-alpha8"},
-     {:prometheus_process_collector, "~> 1.0.0-alpha8", optional: true}]
+     {:prometheus_ex, "~> 1.0.0-rc1"},
+     {:prometheus_process_collector, "~> 1.0.0-rc1", optional: true}]
   end
 end
